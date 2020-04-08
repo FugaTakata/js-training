f7ca552a025274a2c893c0a11f5600ca0865edba
FugaTakata <maple1213goo@gmail.com>
2020-04-08 15:16:45 +0900

    solved

--- src/pages/Lesson03/index.js
+++ src/pages/Lesson03/index.js
@@ -1,30 +1,30 @@
-import React from "react";
-import LessonPage from "../../components/LessonPage";
-import Chart from "../../components/Chart03";
-import instruction from "./instruction.md";
+import React from 'react'
+import LessonPage from '../../components/LessonPage'
+import Chart from '../../components/Chart03'
+import instruction from './instruction.md'
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
-};
+  return input.filter((e) => e.gender === '男性') // ここを作りましょう！
+}
 
 const Lesson = () => {
   return (
     <LessonPage
-      answerUrl="/answer03"
-      dataUrl="data/size-and-weight.json"
+      answerUrl='/answer03'
+      dataUrl='data/size-and-weight.json'
       convertData={(input) => {
         return [
           {
-            id: "男性",
+            id: '男性',
             data: convertData(input),
           },
-        ];
+        ]
       }}
       instruction={instruction}
-      title="Lesson 03"
+      title='Lesson 03'
       Chart={Chart}
     />
-  );
-};
+  )
+}
 
-export default Lesson;
+export default Lesson

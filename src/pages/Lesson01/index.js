b5c41d6d3f47531885cefe020632bfbddbf9ad40
FugaTakata <maple1213goo@gmail.com>
2020-04-08 15:14:44 +0900

    solved

--- src/pages/Lesson01/index.js
+++ src/pages/Lesson01/index.js
@@ -1,23 +1,23 @@
-import React from "react";
-import LessonPage from "../../components/LessonPage";
-import Chart from "../../components/Chart01";
-import instruction from "./instruction.md";
+import React from 'react'
+import LessonPage from '../../components/LessonPage'
+import Chart from '../../components/Chart01'
+import instruction from './instruction.md'
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
-};
+  return input.map(([name, count]) => ({ name, count })) // ここを作りましょう！
+}
 
 const Lesson = () => {
   return (
     <LessonPage
-      answerUrl="/answer01"
+      answerUrl='/answer01'
       convertData={convertData}
-      dataUrl="data/chs-capacity.json"
+      dataUrl='data/chs-capacity.json'
       instruction={instruction}
-      title="Lesson 01"
+      title='Lesson 01'
       Chart={Chart}
     />
-  );
-};
+  )
+}
 
-export default Lesson;
+export default Lesson

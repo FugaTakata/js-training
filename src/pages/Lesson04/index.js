73bebe417184439bc5ecd41762c30bedf4f15781
FugaTakata <maple1213goo@gmail.com>
2020-04-08 17:15:06 +0900

    my ans works but not good

--- src/pages/Lesson04/index.js
+++ src/pages/Lesson04/index.js
@@ -1,23 +1,43 @@
-import React from "react";
-import LessonPage from "../../components/LessonPage";
-import Chart from "../../components/Chart04";
-import instruction from "./instruction.md";
+import React from 'react'
+import LessonPage from '../../components/LessonPage'
+import Chart from '../../components/Chart04'
+import instruction from './instruction.md'
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
-};
+  // const id = []
+  // input.map(({ species }) => {
+  //   if (!id.includes(species)) {
+  //     id.push(species)
+  //   }
+  // })
+  // const data = id.map((item) => ({ id: item, data: [] }))
+  // input.map((item) => {
+  //   for (const d of data) {
+  //     if (d.id === item.species) {
+  //       d.data.push({ x: item.sepalLength, y: item.petalWidth })
+  //     }
+  //   }
+  // })
+  const species = Array.from(new Set(input.map(({ species }) => species)))
+  return species.map((species) => ({
+    id: species,
+    data: input
+      .filter((item) => item.species === species)
+      .map(({ sepalLength: x, petalWidth: y }) => ({ x, y })),
+  })) // ここを作りましょう！
+}
 
 const Lesson = () => {
   return (
     <LessonPage
-      answerUrl="/answer04"
-      dataUrl="data/iris.json"
+      answerUrl='/answer04'
+      dataUrl='data/iris.json'
       convertData={convertData}
       instruction={instruction}
-      title="Lesson 04"
+      title='Lesson 04'
       Chart={Chart}
     />
-  );
-};
+  )
+}
 
-export default Lesson;
+export default Lesson

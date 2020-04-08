ceaee912096268d3a59fa2d5aee4b2884df8ee9b
FugaTakata <maple1213goo@gmail.com>
2020-04-08 15:15:06 +0900

    solved

--- src/pages/Lesson02/index.js
+++ src/pages/Lesson02/index.js
@@ -1,23 +1,23 @@
-import React from "react";
-import LessonPage from "../../components/LessonPage";
-import Chart from "../../components/Chart02";
-import instruction from "./instruction.md";
+import React from 'react'
+import LessonPage from '../../components/LessonPage'
+import Chart from '../../components/Chart02'
+import instruction from './instruction.md'
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
-};
+  return input.sort((a, b) => b.count - a.count).slice(0, 20) // ここを作りましょう！
+}
 
 const Lesson = () => {
   return (
     <LessonPage
-      dataUrl="data/qiita-tags.json"
-      answerUrl="/answer02"
+      dataUrl='data/qiita-tags.json'
+      answerUrl='/answer02'
       convertData={convertData}
       instruction={instruction}
-      title="Lesson 02"
+      title='Lesson 02'
       Chart={Chart}
     />
-  );
-};
+  )
+}
 
-export default Lesson;
+export default Lesson

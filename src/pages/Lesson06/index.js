d80ca382ac145c67774066218e3496d8a2c52c6a
FugaTakata <maple1213goo@gmail.com>
2020-04-09 18:29:42 +0900

    solved

--- src/pages/Lesson06/index.js
+++ src/pages/Lesson06/index.js
@@ -1,23 +1,34 @@
-import React from "react";
-import LessonPage from "../../components/LessonPage";
-import Chart from "../../components/Chart06";
-import instruction from "./instruction.md";
+import React from 'react'
+import LessonPage from '../../components/LessonPage'
+import Chart from '../../components/Chart06'
+import instruction from './instruction.md'
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
-};
+  const genders = Array.from(new Set(input.map(({ gender }) => gender)))
+  const colors = {
+    男性: 'blue',
+    女性: 'red',
+  }
+  return input.map(({ gender, x: weight, y: height }) => ({
+    color: colors[gender],
+    gender,
+    bmi: weight / (height / 100) ** 2,
+    weight,
+    height,
+  })) // ここを作りましょう！
+}
 
 const Lesson = () => {
   return (
     <LessonPage
-      answerUrl="/answer06"
+      answerUrl='/answer06'
       convertData={convertData}
-      dataUrl="data/size-and-weight.json"
+      dataUrl='data/size-and-weight.json'
       instruction={instruction}
-      title="Lesson 06"
+      title='Lesson 06'
       Chart={Chart}
     />
-  );
-};
+  )
+}
 
-export default Lesson;
+export default Lesson
